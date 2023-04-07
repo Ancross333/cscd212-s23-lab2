c8ffe8fa838f137c6b41155b6be30ff67481fe3c
Patrick <p_shoffner@outlook.com>
2023-04-07 11:39:54 -0700

    Finish Comparators

--- src/cscd212comparators/lab2/TelevisionResolutionMakeDescendingComparator.java
+++ src/cscd212comparators/lab2/TelevisionResolutionMakeDescendingComparator.java
@@ -1,4 +1,19 @@
 package cscd212comparators.lab2;
 
-public class TelevisionResolutionMakeDescendingComparator {
+import cscd212classes.lab2.Television;
+
+import java.util.Comparator;
+
+public class TelevisionResolutionMakeDescendingComparator implements Comparator<Television> {
+
+    public int compare(Television t1, Television t2){
+        if(t1 == null || t2 == null)
+            throw new IllegalArgumentException("One or more null Television objects");
+
+        if(t1.getResolution() - t2.getResolution() == 0){
+            return t1.getMake().compareTo(t2.getMake());
+        }
+
+        return t1.getResolution() - t2.getResolution();
+    }
 }
